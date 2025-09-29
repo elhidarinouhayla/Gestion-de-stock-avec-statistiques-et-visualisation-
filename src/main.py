1e49c661c164d9ef5dbdfa605e2eaa43d0bdc187
KarimaChami <karimachami24@gmail.com>
2025-09-29 12:26:07 +0100

    Add data

---
+++ src/main.py
@@ -0,0 +1,6 @@
+data = [
+    ["Parfum cheveux luxe", 10, 150],
+    ["Crème corps au safran et musc", 20, 200],
+    ["Crème mains et pieds au musc", 15, 120],
+    ["Hydratant cheveux au clou de girofle", 25, 90]
+]
