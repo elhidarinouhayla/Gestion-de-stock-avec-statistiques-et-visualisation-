e0c5f1459e085ad58f0dcb83ca797620e61f8376
KarimaChami <karimachami24@gmail.com>
2025-09-30 12:50:30 +0100

    main

---
+++ src/main.py
@@ -0,0 +1,30 @@
+# # from menu import *
+# from stock import *
+# # from stats import *
+# from visualize import *
+
+
+# def main():
+#     while True:
+#         produits_dispo()
+#         choice = 22  # menu()
+
+#         if choice == "1":
+#             add_product()
+#         elif choice == "2":
+#            supprimer_product()
+#         elif choice == "3":
+#             update_produit()
+#         elif choice == "4":
+#             graphique_barres()
+#         elif choice == "5":
+#             Camembert_diagramme()
+#         elif choice == "0":
+#             print("Exiting...")
+#             break
+#         else:
+#             print("Invalid choice. choisir une option.")
+
+# if __name__ == "__main__":
+#     main()
+
